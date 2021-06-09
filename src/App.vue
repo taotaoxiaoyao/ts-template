8b81ab16235f924dc35d9a349e6510dac8d2abbe
nan.zhang <576274890@qq.com>
2021-06-09 23:54:49 +0800

    touchmove

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
     <div class="demo">
-      <div class="app-main">
-        <ul class="img-list">
+      <div class="app-main" ref="parent" @touchmove.prevent="onMove">
+        <ul class="img-list" first="0">
           <li
             v-for="(item, index) in topItem"
             :ref="`canvas${index}`"
             :key="JSON.stringify(item) + index"
             :style="{ backgroundColor: item.bg }"
             group="gpt"
-            :left="item.left"
-            :top="item.top"
-            :sel="item.sel"
-            :check="item.check"
+            left="0"
+            top="0"
+            sel="0"
+            check="0"
+            orderCheck="0"
+            pair="0"
             class="list-item"
-            @touchstart.stop.prevent="onStart"
+            @touchstart.prevent="onStart"
           >
             <canvas class="img-canvas" width="100%" height="100%"></canvas>
           </li>
@@ -25,18 +27,21 @@
             :key="JSON.stringify(item) + index"
             :ref="`b${index}`"
             group="gpb"
-            :left="item.left"
-            :top="item.top"
-            :sel="item.sel"
-            :check="item.check"
+            left="0"
+            top="0"
+            sel="0"
+            check="0"
+            orderCheck="0"
+            pair="0"
             class="list-item"
+            @touchstart.prevent="onStart"
           >
             <p class="txt">{{ item.text }}</p>
           </li>
         </ul>
         <!-- -->
-        <canvas class="canvas"></canvas>
-        <canvas class="backcanvas"></canvas>
+        <canvas class="canvas" ref="canvas"></canvas>
+        <canvas class="backCanvas" ref="backCanvas"></canvas>
       </div>
     </div>
     <!-- <router-view/> -->
@@ -45,9 +50,29 @@
 
 <script lang="ts">
 import Vue from 'vue'
+// interface Item {
+//   bg?: string
+//   text?: string
+// }
 export default Vue.extend({
   data() {
     return {
+      canvas: null as unknown as HTMLCanvasElement,
+      backCanvas: null as unknown as HTMLCanvasElement,
+      isBegin: false,
+      curStart: null as unknown as HTMLElement,
+      startX: null as unknown,
+      startY: null as unknown,
+      midX: null as unknown,
+      midY: null as unknown,
+      endX: null as unknown,
+      endY: null as unknown,
+      parent: {
+        offsetLeft: 0,
+        offsetTop: 0,
+        offsetWidth: 0,
+        offsetHeight: 0
+      },
       topItem: [
         {
           bg: 'antiquewhite'
@@ -79,31 +104,115 @@ export default Vue.extend({
     }
   },
   mounted() {
+    this.setParent()
+    this.setCanvas()
     this.setAttr()
   },
   methods: {
+    setParent() {
+      const parent = this.$refs['parent'] as HTMLElement
+      this.parent = {
+        offsetLeft: parent.offsetLeft,
+        offsetTop: parent.offsetTop,
+        offsetWidth: parent.offsetWidth,
+        offsetHeight: parent.offsetHeight
+      }
+    },
+    setCanvas() {
+      this.canvas = this.$refs['canvas'] as HTMLCanvasElement
+      this.backCanvas = this.$refs['backCanvas'] as HTMLCanvasElement
+      this.canvas.width = this.parent.offsetWidth
+      this.canvas.height = this.parent.offsetHeight
+      this.backCanvas.width = this.parent.offsetWidth
+      this.backCanvas.height = this.parent.offsetHeight
+    },
     setAttr(): void {
       this.topItem.forEach((item, index) => {
-        const liArr: HTMLElement[] = this.$refs[
-          `canvas${index}`
-        ] as HTMLElement[]
-        const li = liArr[0]
-        this.$set(item, 'left', li.offsetLeft + li.offsetWidth / 2)
-        this.$set(item, 'top', li.offsetTop + li.offsetHeight)
-        this.$set(item, 'sel', 0)
-        this.$set(item, 'check', 0)
+        this.attr(`canvas${index}`)
       })
       this.bItem.forEach((item, index) => {
-        const liArr: HTMLElement[] = this.$refs[`b${index}`] as HTMLElement[]
-        const li = liArr[0]
-        this.$set(item, 'left', li.offsetLeft + li.offsetWidth / 2)
-        this.$set(item, 'top', li.offsetTop)
-        this.$set(item, 'sel', 0)
-        this.$set(item, 'check', 0)
+        this.attr(`b${index}`)
       })
     },
+    attr(id: string) {
+      const liArr: HTMLElement[] = this.$refs[id] as HTMLElement[]
+      const li = liArr[0]
+      const top = id.includes('canvas')
+        ? li.offsetTop + li.offsetHeight
+        : li.offsetTop
+      li?.setAttribute('left', (li.offsetLeft + li.offsetWidth / 2).toString())
+      li?.setAttribute('top', top.toString())
+    },
     onStart(e: TouchEvent): void {
-      console.log('e: ', e)
+      const touches = e.touches
+      if (touches.length > 1) {
+        return
+      }
+      const cur = touches[0]
+      const curElem = cur.target as HTMLElement
+      const parent = curElem.parentElement as HTMLElement
+
+      if (!parent?.className.includes('list-item')) {
+        return
+      }
+      const pp = parent?.parentElement
+      this.curStart = curElem
+      this.isBegin = true
+      pp?.setAttribute('first', '1')
+      parent?.setAttribute('sel', '1')
+      parent.className = 'list-item addStyle'
+      this.startX = parent?.getAttribute('left')
+      this.startY = parent?.getAttribute('top')
+    },
+    onMove(e: TouchEvent): void {
+      var touch = e.touches[0]
+      const cur = document.elementFromPoint(touch.clientX, touch.clientY)
+      if (this.isBegin) {
+        this.midX = touch.pageX - this.parent.offsetLeft
+        this.midY = touch.pageY - this.parent.offsetTop
+      }
+      const parent = cur?.parentElement as HTMLElement
+      const pp = parent?.parentElement as HTMLElement
+      if (cur?.className.includes('app-main')) {
+        console.log('在canvas外move')
+        // 		if (part1.attr("first") == 0) {
+        // 			part1.children("span").each(function (index, element) {
+        // 				if ($(this).attr('check') == 0) {
+        // 					$(this).attr("sel", "0").removeClass("addstyle");
+        // 				};
+        // 			});
+        // 		};
+        // 		if (part2.attr("first") == 0) {
+        // 			part2.children("span").each(function (index, element) {
+        // 				if ($(this).attr('check') == 0) {
+        // 					$(this).attr("sel", "0").removeClass("addstyle");
+        // 				};
+        // 			});
+        // 		};
+      } else {
+        if (parseInt(parent?.getAttribute('check') as string) === 0) {
+          parent.className = 'list-item addStyle'
+          parent.setAttribute('sel', '1')
+          const list = pp.children //获取父级的所有子节点
+          for (let i = 0; i < list.length; i++) {
+            //循环
+            const node = list[i]
+            if (
+              node.nodeType == 1 &&
+              node != parent &&
+              node.getAttribute('check') === '0'
+            ) {
+              //如果该节点是元素节点与不是这个节点本身
+              node.className = 'list-item'
+              node.setAttribute('sel', '0')
+            }
+          }
+          if (parseInt(pp?.getAttribute('first') as string) === 1) {
+            this.midX = parent.getAttribute('left')
+            this.midY = parent.getAttribute('top')
+          }
+        }
+      }
     }
   }
 })
@@ -141,16 +250,19 @@ export default Vue.extend({
   color: #666;
   display: block;
 }
-.app-main .frontCanvas,
+.app-main .canvas,
 .app-main .backCanvas {
   position: absolute;
   left: 0;
   top: 0;
 }
-.app-main .frontCanvas {
+.app-main .canvas {
   z-index: -1;
 }
 .app-main .backCanvas {
   z-index: -2;
+}
+.list-item.addStyle {
+  background-color: red !important;
 }
 </style>
