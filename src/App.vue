2d03f85081e7715f25705d5a26ae26e90d9daf9c
nan.zhang <576274890@qq.com>
2021-06-10 22:59:02 +0800

    move

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="demo">
       <div class="app-main" ref="parent" @touchmove.prevent="onMove">
-        <ul class="img-list" first="0">
+        <ul class="img-list" first="0" ref="top">
           <li
             v-for="(item, index) in topItem"
             :ref="`canvas${index}`"
@@ -21,7 +21,7 @@
             <canvas class="img-canvas" width="100%" height="100%"></canvas>
           </li>
         </ul>
-        <ul class="txt-list">
+        <ul class="txt-list" first="0" ref="bottom">
           <li
             v-for="(item, index) in bItem"
             :key="JSON.stringify(item) + index"
@@ -58,15 +58,19 @@ export default Vue.extend({
   data() {
     return {
       canvas: null as unknown as HTMLCanvasElement,
+      cContext: null as unknown as CanvasRenderingContext2D,
+      bContext: null as unknown as CanvasRenderingContext2D,
       backCanvas: null as unknown as HTMLCanvasElement,
+      linewidth: 2,
+      linestyle: '#0C6',
       isBegin: false,
       curStart: null as unknown as HTMLElement,
-      startX: null as unknown,
-      startY: null as unknown,
-      midX: null as unknown,
-      midY: null as unknown,
-      endX: null as unknown,
-      endY: null as unknown,
+      startX: null as unknown as number,
+      startY: null as unknown as number,
+      midX: null as unknown as number,
+      midY: null as unknown as number,
+      endX: null as unknown as number,
+      endY: null as unknown as number,
       parent: {
         offsetLeft: 0,
         offsetTop: 0,
@@ -121,6 +125,10 @@ export default Vue.extend({
     setCanvas() {
       this.canvas = this.$refs['canvas'] as HTMLCanvasElement
       this.backCanvas = this.$refs['backCanvas'] as HTMLCanvasElement
+      this.cContext = this.canvas.getContext('2d') as CanvasRenderingContext2D
+      this.bContext = this.backCanvas.getContext(
+        '2d'
+      ) as CanvasRenderingContext2D
       this.canvas.width = this.parent.offsetWidth
       this.canvas.height = this.parent.offsetHeight
       this.backCanvas.width = this.parent.offsetWidth
@@ -161,8 +169,8 @@ export default Vue.extend({
       pp?.setAttribute('first', '1')
       parent?.setAttribute('sel', '1')
       parent.className = 'list-item addStyle'
-      this.startX = parent?.getAttribute('left')
-      this.startY = parent?.getAttribute('top')
+      this.startX = parent?.getAttribute('left') as unknown as number
+      this.startY = parent?.getAttribute('top') as unknown as number
     },
     onMove(e: TouchEvent): void {
       var touch = e.touches[0]
@@ -170,49 +178,62 @@ export default Vue.extend({
       if (this.isBegin) {
         this.midX = touch.pageX - this.parent.offsetLeft
         this.midY = touch.pageY - this.parent.offsetTop
-      }
-      const parent = cur?.parentElement as HTMLElement
-      const pp = parent?.parentElement as HTMLElement
-      if (cur?.className.includes('app-main')) {
-        console.log('在canvas外move')
-        // 		if (part1.attr("first") == 0) {
-        // 			part1.children("span").each(function (index, element) {
-        // 				if ($(this).attr('check') == 0) {
-        // 					$(this).attr("sel", "0").removeClass("addstyle");
-        // 				};
-        // 			});
-        // 		};
-        // 		if (part2.attr("first") == 0) {
-        // 			part2.children("span").each(function (index, element) {
-        // 				if ($(this).attr('check') == 0) {
-        // 					$(this).attr("sel", "0").removeClass("addstyle");
-        // 				};
-        // 			});
-        // 		};
-      } else {
-        if (parseInt(parent?.getAttribute('check') as string) === 0) {
-          parent.className = 'list-item addStyle'
-          parent.setAttribute('sel', '1')
-          const list = pp.children //获取父级的所有子节点
-          for (let i = 0; i < list.length; i++) {
-            //循环
-            const node = list[i]
-            if (
-              node.nodeType == 1 &&
-              node != parent &&
-              node.getAttribute('check') === '0'
-            ) {
-              //如果该节点是元素节点与不是这个节点本身
-              node.className = 'list-item'
-              node.setAttribute('sel', '0')
+        const parent = cur?.parentElement as HTMLElement
+        const pp = parent?.parentElement as HTMLElement
+        if (cur?.className.includes('app-main')) {
+          const top = this.$refs['top'] as HTMLElement
+          const bottom = this.$refs['bottom'] as HTMLElement
+          this.setPP(top)
+          this.setPP(bottom)
+        } else {
+          if (parseInt(parent?.getAttribute('check') as string) === 0) {
+            const list = pp.children //获取父级的所有子节点
+            for (let i = 0; i < list.length; i++) {
+              const node = list[i]
+              if (node.nodeType == 1 && node.getAttribute('check') === '0') {
+                //如果该节点是元素节点与不是这个节点本身
+                node.className = 'list-item'
+                node.setAttribute('sel', '0')
+              }
+            }
+            parent.className = 'list-item addStyle'
+            parent.setAttribute('sel', '1')
+            if (parseInt(pp?.getAttribute('first') as string) === 1) {
+              this.startX = parent.getAttribute('left') as unknown as number
+              this.startY = parent.getAttribute('top') as unknown as number
             }
           }
-          if (parseInt(pp?.getAttribute('first') as string) === 1) {
-            this.midX = parent.getAttribute('left')
-            this.midY = parent.getAttribute('top')
-          }
         }
+        this.backstrockline()
       }
+    },
+    setPP(arr: HTMLElement) {
+      if (arr.getAttribute('first') === '0') {
+        const children = arr.children as unknown as HTMLElement[]
+        children.forEach(item => {
+          if (item.getAttribute('check') === '0') {
+            item.setAttribute('sel', '0')
+            item.className = 'list-item'
+          }
+        })
+      }
+    },
+    backstrockline() {
+      //绘制
+      this.bContext.clearRect(
+        0,
+        0,
+        this.parent.offsetWidth,
+        this.parent.offsetHeight
+      )
+      this.bContext.save()
+      this.bContext.beginPath()
+      this.bContext.lineWidth = this.linewidth
+      this.bContext.moveTo(this.startX, this.startY)
+      this.bContext.lineTo(this.midX, this.midY)
+      this.bContext.strokeStyle = this.linestyle
+      this.bContext.stroke()
+      this.bContext.restore()
     }
   }
 })
