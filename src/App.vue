3a1df376c561ae3ba4d60480e4a383b74bb7d665
nan.zhang <576274890@qq.com>
2021-06-18 00:11:21 +0800

    end

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <div class="demo">
-      <div class="app-main" ref="parent" @touchmove.prevent="onMove">
+      <div
+        class="app-main"
+        ref="parent"
+        @touchmove.prevent="onMove"
+        @touchend.prevent="onEnd"
+      >
         <ul class="img-list" first="0" ref="top">
           <li
             v-for="(item, index) in topItem"
@@ -14,7 +19,6 @@
             sel="0"
             check="0"
             orderCheck="0"
-            pair="0"
             class="list-item"
             @touchstart.prevent="onStart"
           >
@@ -32,7 +36,6 @@
             sel="0"
             check="0"
             orderCheck="0"
-            pair="0"
             class="list-item"
             @touchstart.prevent="onStart"
           >
@@ -50,13 +53,26 @@
 
 <script lang="ts">
 import Vue from 'vue'
-// interface Item {
-//   bg?: string
-//   text?: string
-// }
+interface DataKey {
+  selTop: string
+  selBottom: string
+  countTop: string
+  countBottom: string
+}
 export default Vue.extend({
   data() {
     return {
+      countTop: 0,
+      countBottom: 0,
+      pairSet: 0,
+      pair: 0,
+      orderCheck: [] as boolean[],
+      orderPair: [] as (string | null)[],
+      mx: [] as number[],
+      my: [] as number[],
+      ms: [] as number[],
+      pairL: [] as number[],
+      pairR: [] as number[],
       canvas: null as unknown as HTMLCanvasElement,
       cContext: null as unknown as CanvasRenderingContext2D,
       bContext: null as unknown as CanvasRenderingContext2D,
@@ -64,7 +80,13 @@ export default Vue.extend({
       linewidth: 2,
       linestyle: '#0C6',
       isBegin: false,
+      top: null as unknown as HTMLElement,
+      bottom: null as unknown as HTMLElement,
+      selTop: null as unknown as HTMLElement,
+      selBottom: null as unknown as HTMLElement,
       curStart: null as unknown as HTMLElement,
+      lastX: null as unknown as number,
+      lastY: null as unknown as number,
       startX: null as unknown as number,
       startY: null as unknown as number,
       midX: null as unknown as number,
@@ -121,6 +143,8 @@ export default Vue.extend({
         offsetWidth: parent.offsetWidth,
         offsetHeight: parent.offsetHeight
       }
+      this.top = this.$refs['top'] as HTMLElement
+      this.bottom = this.$refs['bottom'] as HTMLElement
     },
     setCanvas() {
       this.canvas = this.$refs['canvas'] as HTMLCanvasElement
@@ -151,7 +175,7 @@ export default Vue.extend({
       li?.setAttribute('left', (li.offsetLeft + li.offsetWidth / 2).toString())
       li?.setAttribute('top', top.toString())
     },
-    onStart(e: TouchEvent): void {
+    onStart(e: TouchEvent) {
       const touches = e.touches
       if (touches.length > 1) {
         return
@@ -163,16 +187,34 @@ export default Vue.extend({
       if (!parent?.className.includes('list-item')) {
         return
       }
+      this.countTop = 0
+      this.countBottom = 0
       const pp = parent?.parentElement
-      this.curStart = curElem
+      this.curStart = parent
+      const li = [...(this.top.children as unknown as HTMLElement[])]
+      li.push(...(this.bottom.children as unknown as HTMLElement[]))
+      console.log('li: ', li, this.bottom)
+      li.forEach((item, index) => {
+        if (item.className.indexOf('addstyle') > -1) {
+          item.setAttribute('orderCheck', '1')
+          this.$set(this.orderPair, index, item.getAttribute('pair'))
+        } else {
+          item.setAttribute('orderCheck', '0')
+          this.$set(this.orderPair, index, null)
+        }
+      })
+
       this.isBegin = true
+      this.pairSet =
+        (this.curStart.getAttribute('pair') as unknown as number) * 2
       pp?.setAttribute('first', '1')
       parent?.setAttribute('sel', '1')
-      parent.className = 'list-item addStyle'
+      parent.className =
+        parent.getAttribute('check') == '0' ? 'list-item addStyle' : 'list-item'
       this.startX = parent?.getAttribute('left') as unknown as number
       this.startY = parent?.getAttribute('top') as unknown as number
     },
-    onMove(e: TouchEvent): void {
+    onMove(e: TouchEvent) {
       var touch = e.touches[0]
       const cur = document.elementFromPoint(touch.clientX, touch.clientY)
       if (this.isBegin) {
@@ -181,10 +223,8 @@ export default Vue.extend({
         const parent = cur?.parentElement as HTMLElement
         const pp = parent?.parentElement as HTMLElement
         if (cur?.className.includes('app-main')) {
-          const top = this.$refs['top'] as HTMLElement
-          const bottom = this.$refs['bottom'] as HTMLElement
-          this.setPP(top)
-          this.setPP(bottom)
+          this.setMovePP(this.top)
+          this.setMovePP(this.bottom)
         } else {
           if (parseInt(parent?.getAttribute('check') as string) === 0) {
             const list = pp.children //获取父级的所有子节点
@@ -207,7 +247,79 @@ export default Vue.extend({
         this.backstrockline()
       }
     },
-    setPP(arr: HTMLElement) {
+    onEnd(e: TouchEvent) {
+      var touch = e.changedTouches[0]
+      console.log('touch: ', e)
+      const cur = document.elementFromPoint(touch.clientX, touch.clientY)
+      if (this.isBegin) {
+        const parent = cur?.parentElement as HTMLElement
+        const pp = parent?.parentElement as HTMLElement
+        if (cur?.className.includes('app-main')) {
+          console.log(111111)
+        } else {
+          if (pp.getAttribute('first') === '0') {
+            if (parent.getAttribute('check') === '0') {
+              if (this.curStart.getAttribute('check') === '1') {
+                this.setEndPP(this.top, 'selTop', 'countTop')
+                this.setEndPP(this.bottom, 'selBottom', 'countBottom')
+                if (this.countTop == 1 && this.countBottom == 1) {
+                  this.mx.push(
+                    this.selTop.getAttribute('left') as unknown as number
+                  )
+                  this.my.push(
+                    this.selTop.getAttribute('top') as unknown as number
+                  )
+                  this.ms.push(0)
+                  this.selTop.setAttribute('pair', this.pair.toString())
+                  this.pairL.push(this.pair)
+                  this.mx.push(
+                    this.selBottom.getAttribute('left') as unknown as number
+                  )
+                  this.my.push(
+                    this.selBottom.getAttribute('top') as unknown as number
+                  )
+                  this.ms.push(1)
+                  this.selBottom.setAttribute('pair', this.pair.toString())
+                  this.pairR.push(this.pair)
+                  this.pair++
+                  this.top.setAttribute('first', '1')
+                  this.bottom.setAttribute('first', '0')
+
+                  const readline = this.pairSet / 2
+                  this.setPEnd(this.top, readline)
+                  this.setPEnd(this.bottom, readline)
+                  this.strockline2()
+                } else {
+                  console.log(222222)
+                  const li = (
+                    this.top.children as unknown as HTMLElement[]
+                  ).concat(this.bottom.children as unknown as HTMLElement[])
+                  li.forEach((item, index) => {
+                    item.setAttribute('sel', '0')
+                    if (item.getAttribute('orderCheck') === '1') {
+                      item.setAttribute('check', '1')
+                      item.setAttribute('pair', this.orderPair[index] as string)
+                      item.className = 'list-item addStyle'
+                    } else {
+                      item.setAttribute('check', '0')
+                      item.className = 'list-item'
+                      item.removeAttribute('pair')
+                    }
+                  })
+                }
+              } else {
+                console.log(333333)
+              }
+            } else {
+              console.log(444444)
+            }
+          } else {
+            console.log(555555)
+          }
+        }
+      }
+    },
+    setMovePP(arr: HTMLElement) {
       if (arr.getAttribute('first') === '0') {
         const children = arr.children as unknown as HTMLElement[]
         children.forEach(item => {
@@ -216,6 +328,29 @@ export default Vue.extend({
             item.className = 'list-item'
           }
         })
+      }
+    },
+    setEndPP<K extends keyof DataKey>(arr: HTMLElement, selId: K, countId: K) {
+      const children = arr.children as unknown as HTMLElement[]
+      children.forEach(item => {
+        if (item.getAttribute('sel') === '1') {
+          item.setAttribute('sel', '0')
+          item.setAttribute('check', '1')
+          this.$set(this, selId, item)
+          let count = this[countId] as number
+          this.$set(this, countId, count++)
+        }
+      })
+    },
+    setPEnd(arr: HTMLElement, readline: number) {
+      const children = arr.children as unknown as HTMLElement[]
+      for (let item of children) {
+        if ((item.getAttribute('pair') as unknown as number) !== readline) {
+          continue
+        }
+        item.className = 'list-item'
+        item.setAttribute('check', '0')
+        item.removeAttribute('pair')
       }
     },
     backstrockline() {
@@ -234,6 +369,40 @@ export default Vue.extend({
       this.bContext.strokeStyle = this.linestyle
       this.bContext.stroke()
       this.bContext.restore()
+    },
+    strockline2() {
+      //绘制方法
+      this.cContext.clearRect(
+        0,
+        0,
+        this.parent.offsetWidth,
+        this.parent.offsetHeight
+      ) //整个画布清除
+      this.cContext.save()
+      this.cContext.beginPath()
+      this.cContext.lineWidth = this.linewidth
+      const clearLine = this.pairSet
+      for (let i = 0; i < this.ms.length; i++) {
+        //遍历绘制
+        if (clearLine == i) {
+          this.mx[i] = null as unknown as number
+          this.my[i] = null as unknown as number
+        }
+        if (clearLine + 1 == i) {
+          this.mx[i] = null as unknown as number
+          this.my[i] = null as unknown as number
+        }
+        this.lastX = this.mx[i]
+        this.lastY = this.my[i]
+        if (this.ms[i] == 0) {
+          this.cContext.moveTo(this.lastX, this.lastY)
+        } else {
+          this.cContext.lineTo(this.lastX, this.lastY)
+        }
+      }
+      this.cContext.strokeStyle = this.linestyle
+      this.cContext.stroke()
+      // this.cContext.restore()
     }
   }
 })
